a480fd46bb4e1aef53e6973d4a7f04f698ff120a
SparshWadhwa <sparshwadhwa14@gmail.com>
2017-06-18 05:01:27 +0530

    Modification

--- app/src/main/java/location/in/myapplication1/FragmentTwo.java
+++ app/src/main/java/location/in/myapplication1/FragmentTwo.java
@@ -2,11 +2,18 @@ package location.in.myapplication1;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class FragmentTwo extends Fragment {
+    ListView listView ;
+    View view;
 
     public FragmentTwo() {
         // Required empty public constructor
@@ -15,13 +22,36 @@ public class FragmentTwo extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+        // Inflate the layout for this fragment
+
+        // Defined Array values to show in ListView
+
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_two, container, false);
+
+
     }
+
 
 }
